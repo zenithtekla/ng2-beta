430715bde0a0ad27937c7bbd8fb07825eccc33d6
Phuc <zenithtekla@gmail.com>
2016-09-07 15:50:38 -0700

    r6 added life-cycle hook through 'class implements OnInit{}' & external HTML

--- dev/shopping-list.component.ts
+++ dev/shopping-list.component.ts
@@ -1,4 +1,8 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
+import {TestComponent} from "./test.component";
+
+// @decorator in TypeScript is an easy way to add metadata to the class.
+// Decorator is the maker of the component, makes ng2 recognize the component
 
 @Component({
    // some component, configuration
@@ -16,21 +20,34 @@ import {Component} from "angular2/core";
       <label for="addItem">Add Item</label><input type="text" #item id="addItem">
       <button (click) = "onAddItem(item)">Add Item</button>
       <br>
-      Hi, I'm {{name}}, Angular2-Meteor <span [class.is-awesome]="inputElement.value==='yes'">is awesome!</span>
+      <br>
+      <hr>
+      <br>
+      <br>
+      Hi, I'm <span [style.color]="inputElement.value==='yes' ? 'olive': ''">{{name}}</span>, Angular2-Meteor <span [class.is-awesome]="inputElement.value==='yes'">is awesome!</span>
       <br>
       <br>
       Is it awesome?
       <input type="text" #inputElement (keyup)="0">
+      <br>
+      <br>
+      <button [disabled]="inputElement.value!=='yes'" >Only enabled if 'yes' was entered</button>
+      <test></test>
   `,
-  styleUrls: ['src/css/custom.css']
+  styleUrls: ['src/css/custom.css'],
+  directives: [TestComponent]
   // styles: ['.class{}'] // for simple inline styling
 
 
   /*  * means, structured directive and is required for condition, method to change the document
       # means local variable
-      parentheses means events   */
+      parentheses means events, listening and getting sth out of an element
+      square brackets [] : change the element, passing sth into the element */
 })
-export class ShoppingListComponent {
+
+// implement is the contract our class
+
+export class ShoppingListComponent implements OnInit {
   public items = [
     {name: "Milk"},
     {name: "Sugar"},
@@ -48,6 +65,12 @@ export class ShoppingListComponent {
     this.items.splice(this.items.indexOf(this.selectedItem),1);
   }
 
-  public name = "Piper";
+  public name: string;
+
+  // Angular2 life-cycle hook
+  ngOnInit(): any {
+    // return null;
+    this.name = "Piper";
+  }
 
 }
---
+++ dev/test.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core'
+import {templateVisitAll} from "angular2/compiler";
+
+@Component({
+  selector: 'test',
+  templateUrl: 'templates/test.tpl.html'
+})
+
+export class TestComponent {
+
+}
