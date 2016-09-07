5cc6eea6e233142379196286e9cbeb0a1551f708
Phuc <zenithtekla@gmail.com>
2016-09-07 10:35:52 -0700

    r2 added an event

--- dev/shopping-list.component.ts
+++ dev/shopping-list.component.ts
@@ -4,9 +4,23 @@ import {Component} from "angular2/core";
    // some component, configuration
   selector: "shopping-list",
   template: `
-    This is the shopping-list
+    <ul>
+    <li 
+      *ngFor="#item of items"
+      (click) = "onItemClicked(item)"
+      >{{ item }}</li>
+    </ul>
+      <input type="text" [(ngModel)]="selectedItem">
   `
+  /*  * means, structured directive and is required for condition, method to change the document
+      # means local variable
+      parentheses means events   */
 })
 export class ShoppingListComponent {
+  public items = ["Milk", "Sugar", "Bread"];
+  public selectedItem;
 
+  onItemClicked(item){
+    this.selectedItem = item;
+  }
 }
