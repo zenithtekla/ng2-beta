2cb5177f5ee398136f750a3648beeac2ad6a2081
Phuc <zenithtekla@gmail.com>
2016-09-07 09:52:28 -0700

    r1 add a component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {ShoppingListComponent} from "./shopping-list.component";
+
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        <shopping-list></shopping-list>
     `,
+    directives: [ShoppingListComponent]
 })
 export class AppComponent {
 
---
+++ dev/shopping-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+
+@Component({
+   // some component, configuration
+  selector: "shopping-list",
+  template: `
+    This is the shopping-list
+  `
+})
+export class ShoppingListComponent {
+
+}
