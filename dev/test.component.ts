757cd2b1e26e65882f2e028eb11fad9a2c587266
Phuc <zenithtekla@gmail.com>
2016-09-09 19:48:28 -0700

    r7 components and ngFor cont.

--- dev/test.component.ts
+++ dev/test.component.ts
@@ -1,11 +1,35 @@
 import {Component} from 'angular2/core'
-import {templateVisitAll} from "angular2/compiler";
 
 @Component({
   selector: 'test',
-  templateUrl: 'templates/test.tpl.html'
+  templateUrl: 'templates/test.tpl.html',
+  styleUrls: ['src/css/app.css']
 })
 
 export class TestComponent {
+  public contacts = [{
+    firstName: "Piper",
+    lastName: "Tanska",
+    phone: "0137 1230 578",
+    email: "pipert@hivecrowd.com"
+  }, {
+    firstName: "Paige",
+    lastName: "Sami",
+    phone: "086768 23668",
+    email: "paiges@hivecrowd.fi"
+  },
+    {
+      firstName: "Prue",
+      lastName: "Korski",
+      phone: "087 1456 235",
+      email: "pruek@hivecrowd.fi"
+    }
+  ];
+  public selectedContact = {};
+  
+  public showDetail = false;
 
+  onSelect(contact){
+    this.selectedContact = contact;
+  }
 }
